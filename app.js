16254e6f059afbdf9cea548313948f2c412ddcf9
Valmir Almeida <almeidavalmir76@gmail.com>
2023-02-23 21:03:28 -0300

    Install morgan & handling errors

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const rotaProdutos = require("./routes/Produtos");
 const rotaPedidos = require("./routes/pedidos");
 
+app.use(morgan("dev"));
+
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
+
+// QUANDO NAO ENCONTRA ROTA, CAI AQUI!
+app.use((req, res, next) => {
+  const erro = new Error("Não encontrado");
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
+});
 
 module.exports = app;
